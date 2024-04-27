f0c5823a23988a00e4c5033e02ba586ff9f1fb42
Nikki Agrawal <46136979+yoitsnikki@users.noreply.github.com>
2024-04-27 17:25:40 -0400

    Update and rename Rockets.java to Rocket.java

--- Rocket.java
+++ Rocket.java
@@ -1,4 +1,6 @@
+
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,13 +11,25 @@ public class Rocket {
         this.x = x;
         this.y = y;
     }
+    public int getXCoord() {
+        return x;
+    }
+    public int getYCoord() {
+        return y;
+    }
+
     //Made this move method assuming that rockets fly from right border of the screen to the left
     public void draw(Graphics g) {
         g.setColor(Color.RED);
         g.fillRect(x, y, 10, 10); // Draw rocket as a red square
     }
+    public Rectangle getRocketBounds() {
+        return new Rectangle(this.getXCoord(),this.getYCoord(),10,10); //Do I need this. keyword?
+    }
+
     public void move() {
         x -= 5; // Move rocket leftward
+        y += 1; // Move rocket downward
     }
     public static void addRocket(){
         rockets.add(new Rocket(1000, 75));
