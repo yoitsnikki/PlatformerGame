ee2faac831bbb817f7bad9bb5e6fe9d421d8eacb
Nikki Agrawal <46136979+yoitsnikki@users.noreply.github.com>
2024-04-27 17:25:51 -0400

    Update platformCanvas.java

--- platformCanvas.java
+++ platformCanvas.java
@@ -3,41 +3,77 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
+public class platformCanvas {
+    static Color c = Color.BLUE; 
+    final static int numOfLevels = 40;
+    final static int maxNumOfPlatforms =3;
+    final static int heightOfPlatform = 30;
+    final static int yCoordInterval = 150; 
+    ArrayList <Rectangle2D> platformsList = new ArrayList<>();
+    public double yCoord = 850;
+    double xCoord= 0;
+    double interval= 0;
+    double width = 0;
 
-public class platformCanvas  {
-    public static void drawRectangle(Graphics g){
-        int m= 7;
-        double xCoord;
-        double yCoord = 850;
-        Randomization rand = new Randomization();
-
-        final int height = 30;
-
-        ArrayList <Rectangle2D> platformsList = new ArrayList<>();
-        Color c = Color.BLUE; 
+    public platformCanvas(){
+        createPlatforms();
+    }
+    public void drawRectangle(Graphics g){
         Graphics2D g2d = (Graphics2D) g;
-
-        //creates an array of leveled platfroms (have the same yCoord)
-        for (int l= 0; l<5; l++){
-            for (int i=0; i<m;i++){
-                double width = Randomization.randomWidth();
-                xCoord = Randomization.randomXCoord();
-                Rectangle2D rect = new Rectangle2D.Double(xCoord, yCoord, width, height);
-                platformsList.add(rect);
-                //xCoord+=300; 
-                //random increment inserted of full random?
-
-            }
-            Randomization.setXCoord(0);
-            yCoord-=200;
-        }
-        g2d.setColor(c );
+        g2d.setColor(c);
         for (Rectangle2D rects: platformsList){
             g2d.fill(rects);
-            g2d.draw(rects); }
-
+            g2d.draw(rects);}
     }
-   
+    // creates an array of Rectange2d objects 
+    public void createPlatforms(){
+        //loop for levels
+        for (int l= 0; l<numOfLevels; l++){
+            //loop for platforms in each level
+            for (int i=0; i<maxNumOfPlatforms;i++){
+                if (i==0){
+                    interval = Randomization.randomInteval();
+                    width = Randomization.width();
+                    xCoord = Randomization.xCoordFirst(interval,width);
+                }
+                else{
+                    interval = Randomization.randomInteval();
+                    width = Randomization.width();
+                    xCoord = Randomization.xCoordNext(interval, width);
+                }
+                Rectangle2D rect = new Rectangle2D.Double(xCoord, yCoord, width, heightOfPlatform);
+                if (rect.getX()>1001){
+                    break;
+                }
+                platformsList.add(rect);
+            }
+            Randomization.setXCoord(0);
+            yCoord-= yCoordInterval;
+        }
+    }
+    //change in each platform yCoord + 1 
+    public void updateY(){
+        double x,y, w,h;
+        for (Rectangle2D platform : platformsList){
+            x = platform.getX();
+            w = platform.getWidth();
+            h = platform.getHeight();
+            y = platform.getY() + 1;
+            platform.setRect( x,  y,  w,  h);
+        }
+    }
+    //for debugging
+    // public void YCoordList (){
+    //     for (Rectangle2D platform : platformsList){
+    //         System.out.println(platform.getBounds2D());
+    //     }
+    // }
+    public static void setColor(Color c2){
+        c = c2;
+    }
+
+    //40 levels for 2 min, 18 sec for all to move down 
+    //34 for all to dissapear
 }
 
  
