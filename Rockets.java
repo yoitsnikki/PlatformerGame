c3ee0cd7636a76966224cfbcc7d29acb2b048c2d
Nikki Agrawal <46136979+yoitsnikki@users.noreply.github.com>
2024-04-27 14:21:05 -0400

    Update Rockets.java

--- Rockets.java
+++ Rockets.java
@@ -1,63 +1,34 @@
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
-import javax.swing.Timer;
 
-/*
-This code uses sample canvas of auto generated for me by ChatGPT, so we still need to readjust stuff for our own canvas
-*/
-public class Rockets {
-
-    /*
-    initialize ArrayList for rockets as part of the game
-    initialize ArrayList for platforms
-    initialize timer that's responsible for sending a rocket once in 5 seconds
-     */
-    private static List<Rocket> rockets = new ArrayList<>();
-    private static Timer timer;
-
-    //class for rockets object
-    private static class Rocket {
-        int x, y;
-
-        public Rocket(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        //Made this move method assuming that rockets fly from right border of the screen to the left
-        public void move() {
-            x -= 5; // Move rocket leftward
-        }
-
-        public void draw(Graphics g) {
-            g.setColor(Color.RED);
-            g.fillRect(x, y, 10, 10); // Draw rocket as a red square
-        }
+public class Rocket {
+    int x, y;
+    public static List<Rocket> rockets = new ArrayList<>();
+    public Rocket(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
-    
-    Timer rocketTimer = new Timer(5000, new Timer1());
-    rocketTimer.start();
-    
-   // component to draw rockets
-    public void drawRockets(Graphics g) {
+    //Made this move method assuming that rockets fly from right border of the screen to the left
+    public void draw(Graphics g) {
+        g.setColor(Color.RED);
+        g.fillRect(x, y, 10, 10); // Draw rocket as a red square
+    }
+    public void move() {
+        x -= 5; // Move rocket leftward
+    }
+    public static void addRocket(){
+        rockets.add(new Rocket(1000, 75));
+        rockets.add(new Rocket(1000, 225));
+        rockets.add(new Rocket(1000, 375));
+        rockets.add(new Rocket(1000, 525));
+        rockets.add(new Rocket(1000, 675));
+        rockets.add(new Rocket(1000, 825));
+
+    }
+    public static void drawRockets(Graphics g) {
         for (Rocket rocket : rockets) {
-                 rocket.draw(g);
-             }
-    }
-
-    //what occurs when the timer is called	
-    class Timer1 implements ActionListener {
-    	public void actionPerformed(ActionEvent e) {
-    		rockets.add(new Rocket(980, 200));
-    	    rockets.add(new Rocket(980, 400));
-    	    rockets.add(new Rocket(980, 600));
-    	    rockets.add(new Rocket(980, 800));
-    	}
+            rocket.draw(g);
+        }
     }
 }
-
-		
