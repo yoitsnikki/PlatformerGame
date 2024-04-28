7b5ae60005181ed917093e40ace83eeaaeac0959
Nikki Agrawal <46136979+yoitsnikki@users.noreply.github.com>
2024-04-28 15:03:05 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,12 +5,22 @@
 
 import javax.swing.JFrame;
 public class Main extends Background{
+	static int screenX = 1000;
+	static int screenY = 1000;
+	
+	public static double getScreenX() {
+		return screenX;
+	}
+	
+	public static double getScreenY() {
+		return screenY;
+	}
 
 	// initiate jframe
 	public static void main(String[] args) {
         JFrame frame = new JFrame("Platformer Game");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1000,1000);
+        frame.setSize(screenX,screenY);
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         Background background = new Background();
